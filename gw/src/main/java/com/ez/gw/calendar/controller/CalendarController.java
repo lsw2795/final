9b7a207911937a28b1e1015937e41d8278c1bc64
Desktop <Desktop@DESKTOP-SBGTSOM>
2023-08-06 21:45:21 +0900

    21:45 일정등록하기

--- gw/src/main/java/com/ez/gw/calendar/controller/CalendarController.java
+++ gw/src/main/java/com/ez/gw/calendar/controller/CalendarController.java
@@ -37,13 +37,38 @@ public class CalendarController {
 		//1
 		logger.info("일정 등록 파라미터 calVo = {}", calVo);
 		
+		String getbegindate = calVo.getBegindate();
+		String getenddate = calVo.getEnddate();
+		
+		String begindate = getbegindate.substring(0,10);
+		String begintime = getbegindate.substring(10);
+		String enddate = getenddate.substring(0,10);
+		String endtime = getenddate.substring(10);
+		
+		calVo.setBegindate(begindate);
+		calVo.setBegintime(begintime);
+		calVo.setEnddate(enddate);
+		calVo.setEndtime(endtime);
+		
+		String alldayFlag = calVo.getAlldayFlag();
+		
+		if(alldayFlag==null || alldayFlag.isEmpty()) {
+			alldayFlag = "N";
+		}else {
+			alldayFlag = "Y";
+		}
+		calVo.setAlldayFlag(alldayFlag);
+		
+		
+		logger.info("vo setting 후 파라미터 calVo = {}", calVo);
+		
 		//2
 		int cnt = calendarService.insertCalendar(calVo);
 		logger.info("일정 등록 결과, cnt = {}", cnt);
 		
 		//3
 		//4
-		return "calendar/calendar";
+		return "redirect:/calendar/calendar";
 	}
 
 }
