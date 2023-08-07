8dfcd9e4f29d3cbecb24c306e744d146b2a25fa5
ezen <ezen@DESKTOP-O03MT84>
2023-08-07 18:00:38 +0900

    스마트에디터추가

--- gw/src/main/java/com/ez/gw/board/controller/NoticeController.java
+++ gw/src/main/java/com/ez/gw/board/controller/NoticeController.java
@@ -3,9 +3,13 @@ package com.ez.gw.board.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ez.gw.board.model.BoardService;
+import com.ez.gw.board.model.BoardVO;
 import com.ez.gw.employee.model.EmployeeService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,12 +22,30 @@ public class NoticeController {
 	private final BoardService boardService;
 	private final EmployeeService employeeService;
 	
-	@GetMapping
+	@GetMapping("/admin/board/noticeWrite")
 	public String noticeWrite_get() {
-		logger.info("공지사항 작성 페이지 보여주기");
+		logger.info("공지사항 글쓰기 페이지 보여주기");
 		
 		return "admin/board/noticeWrite";
 	}
 	
+	@PostMapping("/admin/board/noticeWrite")
+	public String noticeWrite_post(@ModelAttribute BoardVO vo, Model model) {
+		//1
+		logger.info("공지사항 글쓰기 페이지, 파라미터 vo={}", vo);
+		
+		//2
+		int cnt=boardService.insertNotice(vo);
+		String msg="공지사항 등록에 실패했습니다.",url="/admin/board/noticeWrite";
+		if(cnt>0) {
+			msg="공지사항 등록이 완료되었습니다.";
+			url="/admin/board/noticeDetail";
+		}
+		//3
+		model.addAttribute("msg", msg);
+		model.addAttribute("url", url);
+		//4
+		return "common/message";
+	}
 	
 }
--- gw/src/main/webapp/views/admin/board/noticeWrite.jsp
+++ gw/src/main/webapp/views/admin/board/noticeWrite.jsp
@@ -2,6 +2,7 @@
     pageEncoding="UTF-8"%>
 <%@ include file='../../inc/adminTop.jsp'%>
 <link rel="stylesheet" href="<c:url value='/css/adminempform.css'/>">
+<script src="<c:url value='/vendors/tinymce/tinymce.min.js'/>"></script>
 <div class="card mb-3">
 	<div class="card-body position-relative admindefault">
 		<div class="row">
@@ -14,19 +15,19 @@
             <div class="card-body py-2 admindefault">
 				<form>
 					<div class="row mb-3 d-flex align-items-center">
-					    <div class="col-md-auto adminempdiv6">
+					    <div class="col-md-auto">
 					        <label class="form-label" for="title">제목</label>
 					    </div>
-					    <div class="col-md-10">
+					    <div class="col-md-11">
 					        <input type="text" class="form-control admindefault" id="title" />
 					    </div>
 					</div>
 					<div class="row mb-3 d-flex align-items-center">
-					    <div class="col-md-auto adminempdiv6">
+					    <div class="col-md-auto">
 							<label class="form-label" for="basic-form-gender">내용</label>
 						</div>
-						<div class="col-md-10 adminspan">
-							<textarea rows="15" cols="150" class="admindefault">네이버 스마트 에디터 대체자리</textarea>
+						<div class="col-md-11 min-vh-50 align-items-center">
+							 <textarea class="tinymce d-none" data-tinymce="data-tinymce" name="content"></textarea>
 						</div>
 					</div>
 					<div class="row mb-3 d-flex align-items-center">
