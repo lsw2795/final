ae88f0d97fdd959e49f6675d050cf4e47894df4b
KIM1515 <kcy1515@gmail.com>
2023-08-19 00:44:36 +0900

    깃갓성완

--- gw/src/main/java/com/ez/gw/employee/model/EmployeeServiceImpl.java
+++ gw/src/main/java/com/ez/gw/employee/model/EmployeeServiceImpl.java
@@ -1,10 +1,9 @@
 package com.ez.gw.employee.model;
 
-import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.mail.HtmlEmail;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.ez.gw.common.SearchVO;
@@ -16,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
 	private final EmployeeDAO employeeDao;
+	private final JavaMailSender mailSender;
 
 	@Override
 	public EmployeeVO selectByEmpNo(int empNo) {
@@ -66,47 +66,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 
 
 	public void sendEmail(EmployeeVO empVo, String div) {
-		// Mail Server 설정
-		String charSet = "utf-8";
-		String hostSMTP = "smtp.gmail.com"; //네이버 이용시 smtp.naver.com
-		String hostSMTPid = "서버 이메일 주소(보내는 사람 이메일 주소)";
-		String hostSMTPpwd = "서버 이메일 비번(보내는 사람 이메일 비번)";
-
-		// 보내는 사람 EMail, 제목, 내용
-		String fromEmail = "보내는 사람 이메일주소(받는 사람 이메일에 표시됨)";
-		String fromName = "COSMOS";
-		String subject = "";
-		String msg = "";
-
-		if(div.equals("findpw")) {
-			subject = "COSMOS 임시 비밀번호 입니다.";
-			msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
-			msg += "<h3 style='color: blue;'>";
-			msg += empVo.getEname() + "님의 임시 비밀번호 입니다. 비밀번호를 변경하여 사용하세요.</h3>";
-			msg += "<p>임시 비밀번호 : ";
-			msg += empVo.getPwd() + "</p></div>";
-		}
-
-		// 받는 사람 E-Mail 주소
-		String mail = empVo.getEmail();
-		try {
-			HtmlEmail email = new HtmlEmail();
-			email.setDebug(true);
-			email.setCharset(charSet);
-			email.setSSL(true);
-			email.setHostName(hostSMTP);
-			email.setSmtpPort(465); //네이버 이용시 587
-
-			email.setAuthentication(hostSMTPid, hostSMTPpwd);
-			email.setTLS(true);
-			email.addTo(mail, charSet);
-			email.setFrom(fromEmail, fromName, charSet);
-			email.setSubject(subject);
-			email.setHtmlMsg(msg);
-			email.send();
-		} catch (Exception e) {
-			System.out.println("메일발송 실패 : " + e);
-		}
+		
 	}
 	
 
