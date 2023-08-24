ff58255fe54d31fde0127e2ed6ad4727ca9a9b01
Desktop <Desktop@DESKTOP-SBGTSOM>
2023-08-24 15:14:28 +0900

    자원 목록 리스트

--- gw/src/main/java/com/ez/gw/reman/controller/RemanController.java
+++ gw/src/main/java/com/ez/gw/reman/controller/RemanController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ez.gw.employee.model.EmployeeService;
 import com.ez.gw.position.controller.PositionController;
@@ -21,53 +22,56 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-	public class RemanController {
+public class RemanController {
 	private static final Logger logger = LoggerFactory.getLogger(RemanController.class);
 	public final RemanService remanService;
 	public final EmployeeService employeeService;
-	
+
 	@GetMapping("/admin/officeProduct/addOfficeProduct")
 	public String get_officeProduct() {
 		logger.info("관리자- 자원 등록 페이지 ");
-		
+
 		return "admin/officeProduct/addOfficeProduct";
 	}
-	
+
 	@PostMapping("/admin/officeProduct/addOfficeProduct")
-	public String post_officeProduct(@ModelAttribute RemanVO remanVo, @RequestParam(required = false) String manager, Model model) {
+	public String post_officeProduct(@ModelAttribute RemanVO remanVo, @RequestParam(required = false) String manager,
+			Model model) {
 		logger.info("관리자 - 자원 등록, 파라미터 remanVo={}, manager={}", remanVo, manager);
-		
+
 		int empNo = employeeService.selectEmpByName(manager);
 		remanVo.setEmpNo(empNo);
 		int cnt = remanService.insertOfficeProduct(remanVo);
 		logger.info("자원 등록 결과, cnt={}", cnt);
-		
+
 		String msg = "", url = "";
-		if(cnt>0) {
+		if (cnt > 0) {
 			msg = "자원 등록이 완료되었습니다.";
 			url = "/admin/officeProduct/officeProductList";
-		}else {
+		} else {
 			msg = "자원 등록 실패";
 			url = "/admin/officeProduct/addOfficeProduct";
 		}
-		
+
 		model.addAttribute("msg", msg);
 		model.addAttribute("url", url);
-		
+
 		return "common/message";
 	}
-	
+
 	@RequestMapping("/admin/officeProduct/officeProductList")
-	public String productList(@RequestParam(required = false)String category, Model model) {
+	public String productList(@RequestParam(required = false) String category, Model model) {
 		logger.info("자원목록 페이지, 파라미터 category={}", category);
+
 		
-		/*
-		 * List<RemanVO> list = remanService.selectOfficeProductByCategory(category);
-		 * logger.info("list.size={}", list.size());
-		 * 
-		 * model.addAttribute("list", list);
-		 */
 		
+		  List<RemanVO> list = remanService.selectOfficeProductByCategory(category);
+		  logger.info("list.size={}", list.size());
+		  
+		  model.addAttribute("list", list);
+		 
+		 
+
 		return "admin/officeProduct/officeProductList";
 	}
 }
