6bb99c58229e904a3a2635ce81bd0d5b0a4c0e5e
changmo <changmo@changmo>
2023-08-26 04:42:49 +0900

    주말에 쪽지 끝낸다 진짜

--- gw/src/main/java/com/ez/gw/messagecontent/model/MessageContentServiceImpl.java
+++ gw/src/main/java/com/ez/gw/messagecontent/model/MessageContentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ez.gw.messagecontent.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,16 @@ public class MessageContentServiceImpl implements MessageContentService{
 	@Override
 	public int insertMessage(MessageContentVO vo) {
 		return messageContentDao.insertMessage(vo);
+	}
+
+	@Override
+	public List<Integer> selectAllReader(int empNo) {
+		return messageContentDao.selectAllReader(empNo);
+	}
+
+	@Override
+	public MessageViewVO selectLastMessageByReader(MessageViewVO mViewVo) {
+		return messageContentDao.selectLastMessageByReader(mViewVo);
 	}
 	
 	
---
+++ gw/src/main/java/com/ez/gw/messagecontent/model/MessageViewVO.java
@@ -0,0 +1,21 @@
+package com.ez.gw.messagecontent.model;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MessageViewVO {
+	private int messageNo;
+	private String messageContent;
+	private Timestamp sendDate;
+	private String delFlag;
+	private	int empNo;
+	private int reader;
+	private String readerDelflag;
+	private Timestamp readDate;
+	private String sendName;
+	private String readName;
+	private String sendImage;
+	private String readImage;
+}
