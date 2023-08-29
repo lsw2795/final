add71585b1f1c6e0d71bc4fd5a710564af9037cd
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2023-08-29 15:04:43 +0900

    Merge branch 'main' of https://github.com/lsw2795/final.git into lsw

--- gw/src/main/java/com/ez/gw/pds/model/PdsServiceImpl.java
+++ gw/src/main/java/com/ez/gw/pds/model/PdsServiceImpl.java
@@ -170,6 +170,7 @@ public class PdsServiceImpl implements PdsService {
 	@Override
 	public List<PdsVO> select24AnonymousImage() {
 		return pdsDao.select24AnonymousImage();
+	}
 	
 	@Override
 	public int clubFiles(PdsVO pdsVo) {
