b2130ff3b28c0d4af5206286428a3c01b1ab88bc
lsw2795 <134687373+lsw2795@users.noreply.github.com>
2023-09-20 17:33:23 +0900

    코드정리

--- gw/src/main/java/com/ez/gw/commute/model/CommuteService.java
+++ gw/src/main/java/com/ez/gw/commute/model/CommuteService.java
@@ -11,16 +11,13 @@ public interface CommuteService {
 	int updateWorkOut(int empNo); //퇴근 시간 업데이트
 	int selectIsWorkIn(int empNo); //당일 출근 기록있는지 확인 메서드
 	int selectIsWorkOut(int empNo); //당일 퇴근 기록있는지 확인 메서드
-	
 	int updateCommuteState(CommuteVO vo); // 오전 9시 이후로 출근하면 근태 상태가 1 지각으로 바뀌는 메서드
 	int updateCommuteStateEalry(int empNo); //오후 6시 이전에 퇴근하면 근태 상태가 5 조퇴로 바뀌는 메서드
 	int selectLateState(int empNo); //지각인지 현재 상태 조회하는 메서드
 	int updateCommuteStateTotal(int empNo); // 지각 + 조퇴면 3으로 바뀌는 메서드
-	
 	int selectCommuteLateCount(@Param("empNo")int empNo, @Param("date")String date); //월별 지각 횟수 조회 메서드
 	int selectCommuteEarlyleaveCount(@Param("empNo")int empNo, @Param("date")String date); //월별 조퇴 횟수 조회 메서드
 	int selectCommuteAttendanceCount(@Param("empNo")int empNo, @Param("date")String date); //월별 출근 횟수 조회 메서드
-	
 	List<Map<String, Object>> selectCommuteMonthByEmpNo(@Param("empNo")int empNo, @Param("date")String date); //해당 사원 월별 근태 기록 전체 조회
 	
 	//------------admin-------------
@@ -30,7 +27,6 @@ public interface CommuteService {
 	int selectAllCommuteLateCount(SearchCommuteVO searchCommuteVO); //전사원 지각횟수 갯수 조회
 	int selectAllCommuteEalryLeaveCount(SearchCommuteVO searchCommuteVO); //전사원 조퇴횟수 갯수 조회
 	int selectAllCommuteAttendanceCount(SearchCommuteVO searchCommuteVO); //전사원 출근횟수 갯수 조회
-	
 	int updateWorkTime(CommuteVO commuteVo); //출퇴근시간 업데이트 메서드
 	CommuteVO selectTodayCommute(int empNo); // 오늘 근태 데이터 조회 메서드
 }
--- gw/src/main/java/com/ez/gw/commute/model/CommuteVO.java
+++ gw/src/main/java/com/ez/gw/commute/model/CommuteVO.java
@@ -10,7 +10,6 @@ public class CommuteVO {
 	private int commuteState; //근태상태 0:정상출근 1:지각 2:조퇴 3:지각+조퇴
 	private int empNo; //사원번호
 	
-	
 	//수정용 변수
 	private String editInTime; //수정할 출근시간
 	private String editOutTime; //퇴근시간
